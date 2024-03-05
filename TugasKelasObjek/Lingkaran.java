908c4a660373ad0fdc5c4b5de5bfc320c1025906
Syahbrina Dinova <121235500+syabiinaaaa@users.noreply.github.com>
2024-03-05 23:39:56 +0700

    upload tugas

--- TugasKelasObjek/Lingkaran.java
+++ TugasKelasObjek/Lingkaran.java
@@ -1,33 +1,30 @@
-package TugasKelasObjek;
+package Tugas_03;
 
 public class Lingkaran {
-	    public static void main(String[] args) {
-	    	Lingkaran l1 = new Lingkaran(5);
-	    	Lingkaran l2 = l1;
-	    	Lingkaran l3 = new Lingkaran(7);
-	    	System.out.println(l1.getJari2());
-	    	System.out.println(l2.getJari2());
-	    	System.out.println(l3.getJari2());
-	    	l2 = null;
-	    	System.out.println(l1.getJari2());
-	    	if (l2 != null) System.out.println(l2.getJari2());
-	    	System.out.println(l3.getJari2());
-	    }
+	 private int jari2;
+	 
+	 public Lingkaran(int jari2) {
+	    this.jari2 = jari2;}
+
+	 public int getJari2() {
+	     return jari2; }
+
+	 public void setJari2(int jari2) {
+	     this.jari2 = jari2;}
+	 
+	 public static void main(String[] args) {
+	    Lingkaran l1 = new Lingkaran(5);
+	    Lingkaran l2 = l1;
+	    Lingkaran l3 = new Lingkaran(7);
 	    
-	    private int jari2;
-
-	    public Lingkaran(int jari2) {
-	        this.jari2 = jari2;
+	    System.out.println(l1.getJari2());
+	    System.out.println(l2.getJari2());
+	    System.out.println(l3.getJari2());
+	    
+	    l1 = null;
+	    l2 = null;
+	    l3 = null;
+	  
 	    }
 
-	    public int getJari2() {
-	        return jari2;
-	    }
-
-	    public void setJari2(int jari2) {
-	        this.jari2 = jari2;
-	    }
 }
-
-
-
