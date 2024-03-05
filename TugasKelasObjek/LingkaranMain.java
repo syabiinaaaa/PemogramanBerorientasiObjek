4357509995dd825e51dd5a279ded83861dad9775
Syahbrina Dinova <121235500+syabiinaaaa@users.noreply.github.com>
2024-03-05 23:40:39 +0700

    upload tugas

--- TugasKelasObjek/LingkaranMain.java
+++ TugasKelasObjek/LingkaranMain.java
@@ -1,17 +1,21 @@
-package TugasKelasObjek;
+package Tugas_03;
 
-public class LingkaranMain {
+public class MainLingkaran {
 	public static void main(String[] args) {
-	   Lingkaran l1 = new Lingkaran(5);
-	   Lingkaran l2 = l1;
-       System.out.println(l1.getJari2()); //5
-       System.out.println(l2.getJari2()); //5
-       ubahJari2(l2);
-       System.out.println(l1.getJari2()); //4
-       System.out.println(l2.getJari2()); //4	   
-	}
+		Lingkaran l1 = new Lingkaran(5);
+		Lingkaran l2 = l1;
+		
+	    System.out.println(l1.getJari2()); 
+	    System.out.println(l2.getJari2()); 
+	    
+	    ubahJari2(l2);
+	    
+	    System.out.println(l1.getJari2()); 
+	    System.out.println(l2.getJari2()); 	   
+		}
 
 	public static void ubahJari2(Lingkaran obj) {
-	   obj.setJari2(4);
-	} 
+	obj.setJari2(4);} 
+
+
 }
