2d7b134ba10867eeb03a6f910d4df7830f3450f8
Syahbrina Dinova <121235500+syabiinaaaa@users.noreply.github.com>
2024-03-13 12:53:24 +0700

    Upload Tugas Pertemuan05

---
+++ Pertemuan05/Gelas.java
@@ -0,0 +1,19 @@
+package Tugas_04;
+
+public class Gelas {
+	
+	private String warna;
+	public Gelas (String w) {
+		
+		warna = w;
+	}
+	
+	void setWarna(String w) {
+		warna = w;
+	}
+	
+	String getWarna() {
+		return warna;
+	}
+
+}
